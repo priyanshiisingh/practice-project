d7e8b05a7d40d3e1ddcd0f1c5349a61853d72caf
priyanshiisingh <priyanshianamika1815@gmail.com>
2021-11-22 22:00:54 +0530

    store and creducers

---
+++ ecommerce/src/reducers/cReducer.js
@@ -0,0 +1,9 @@
+import { combineReducers } from "redux";
+import auth from "./authReducer";
+import loader from "./loaderReducer";
+
+const combineReducers = () => {
+  auth, loader;
+};
+
+export default combineReducers;
---
+++ ecommerce/src/store.js
@@ -0,0 +1,7 @@
+import { createStore } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import rootReducer from "./reducers/cReducer";
+
+const initialState = {};
+
+const store = createStore(rootReducer, initialState, composeWithDevTools());
