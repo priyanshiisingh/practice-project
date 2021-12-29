084161c4388e6ccf0579b13a7c2cc70e0d89a8e7
priyanshiisingh <priyanshianamika1815@gmail.com>
2021-12-29 17:05:25 +0530

    changes

---
+++ ecommerce/src/reducers/index.js
@@ -0,0 +1,12 @@
+import { combineReducers } from "redux";
+import auth from "./authReducer";
+import loader from "./loaderReducer";
+import products from "./productsReducer";
+import categories from "./categoriesReducer";
+
+export default combineReducers({
+  auth,
+  loader,
+  products,
+  categories,
+});
